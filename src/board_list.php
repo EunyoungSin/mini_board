34658af2b908ac431b5d7e75decfceb53572569c
EunyoungSin <ey5742@gmail.com>
2023-04-11 18:11:19 +0900

    0411

--- src/board_list.php
+++ src/board_list.php
@@ -2,36 +2,38 @@
     define( "DOC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/" );
     define( "URL_DB", DOC_ROOT."mini_board/src/common/db_common.php" );
     include_once ( URL_DB );
-    $http_method = $_SERVER["REQUEST_METHOD"];
 
-    if( array_key_exists( "page_num", $_GET ) )
-    {
-        $page_num = $_GET["page_num"];
-    }
-    else
-    {
-        $page_num = 1;
-    }
+	// GET 체크
+	if( array_key_exists( "page_num", $_GET ) )
+	{
+		$page_num = $_GET["page_num"];
+	}
+	else
+	{
+		$page_num = 1;
+	}
 
-    $limit_num = 5;
+	$limit_num = 5;
 
-    // 게시판 정보 테이블 전체 카운트 획득
-    $result_cnt = select_board_info_cnt();
+	// 게시판 정보 테이블 전체 카운트 획득
+	$result_cnt = select_board_info_cnt();
 
-    // max page number
-    $max_page_num = ceil( (int)$result_cnt[0]["cnt"] / $limit_num );
+	// max page number
+	$max_page_num = ceil( (int)$result_cnt[0]["cnt"] / $limit_num );
 
-    // offset
-    $offset = ( $page_num * $limit_num ) - $limit_num;
+	// offset
+	$offset = ( $page_num * $limit_num ) - $limit_num;
 
-    $arr_prepare = 
-        array(
-            "limit_num" => $limit_num
-            ,"offset" => $offset
-        );
-    $result_paging = select_board_info_paging( $arr_prepare );
-    // var_dump( $max_page_num );
+	$arr_prepare =
+		array(
+			"limit_num"	=> $limit_num
+			,"offset"	=> $offset
+		);
+
+	// 페이징용 데이터 검색
+	$result_paging = select_board_info_paging( $arr_prepare );
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ko">
@@ -44,9 +46,10 @@
     <link href="./css/css.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<section class="notice">
     <p><h1>게시판</h1></p>
-    <hr>
     <table class="table table-striped">
+        <hr>
         <thead>
             <tr>
                 <th>게시글 번호</th>
@@ -61,7 +64,7 @@
             ?>
                 <tr>
                 <td><?php echo $recode["board_no"] ?></td>
-                <td><?php echo $recode["board_title"] ?></td>
+                <td><a href="board_update.php?board_no=<?php echo $recode["board_no"] ?>"><?php echo $recode["board_title"] ?></a></td>
                 <td><?php echo $recode["board_write_date"] ?></td>
                 </tr>
             <?php
@@ -69,13 +72,17 @@
             ?>
         </tbody>
     </table>
+    <div class="d-flex justify-content-center">
     <?php
         for( $i = 1; $i <= $max_page_num; $i++ )
         {
     ?>
-        <a href='board_list.php?page_num=<?php echo $i ?>'><?php echo $i ?></a>
+        <a href='board_list.php?page_num=<?php echo $i ?>' class='btn btn-dark btn-sm'><?php echo $i ?></a>
     <?php
         }
     ?>
+    </div>
+    <button type="submit" class="btn btn-dark" id="write">글쓰기</button>
+</section>
 </body>
 </html>
--- src/common/db_common.php
+++ src/common/db_common.php
@@ -1,119 +1,215 @@
 <?php
 
+// ---------------------------------
+// 함수명	: db_conn
+// 기능		: DB Connection
+// 파라미터	: Obj	&$param_conn
+// 리턴값	: 없음
+// ---------------------------------
 function db_conn( &$param_conn )
 {
-    $host = "localhost";
-    $user = "root";
-    $pass = "root506";
-    $charset = "utf8mb4";
-    $db_name = "board";
-    $dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
-    $pdo_option =
-        array(
-            PDO::ATTR_EMULATE_PREPARES      => false
-            ,PDO::ATTR_ERRMODE              => PDO::ERRMODE_EXCEPTION
-            ,PDO::ATTR_DEFAULT_FETCH_MODE   => PDO::FETCH_ASSOC
-        );
-
-    try
-    {
-        $param_conn = new PDO( $dns, $user, $pass, $pdo_option );
-    }
-    catch( Exception $e )
-    {
-        $param_conn = null;
-        throw new Exception ( $e->getMessage() );
-    }
+	$host = "localhost";
+	$user = "root";
+	$pass = "root506";
+	$charset = "utf8mb4";
+	$db_name = "board";
+	$dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
+	$pdo_option =
+		array(
+			PDO::ATTR_EMULATE_PREPARES		=> false
+			,PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION
+			,PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC
+		);
+	
+	try
+	{
+		$param_conn = new PDO( $dns, $user, $pass, $pdo_option );
+	}
+	catch( Exception $e )
+	{
+		$param_conn = null;
+		throw new Exception( $e->getMessage() );
+	}
 }
 
+// ---------------------------------
+// 함수명	: select_board_info_paging
+// 기능		: 페이징_게시판 정보 검색
+// 파라미터	: Array		&$param_arr
+// 리턴값	: Array		$result
+// ---------------------------------
 function select_board_info_paging( &$param_arr )
 {
-    $sql =
-        " SELECT "
-        ."  board_no "
-        ."  ,board_title "
-        ."  ,board_write_date "
-        ." FROM "
-        ."  board_info "
-        ." WHERE "
-        ."  board_del_flg = '0' "
-        ." ORDER BY "
-        ."  board_no DESC "
-        ." LIMIT :limit_num OFFSET :offset "
-        ;
+	$sql =
+		" SELECT "
+		." 	board_no "
+		." 	,board_title "
+		." 	,board_write_date "
+		." FROM "
+		." 	board_info "
+		." WHERE "
+		." 	board_del_flg = '0' "
+		." ORDER BY "
+		." 	board_no DESC "
+		." LIMIT :limit_num OFFSET :offset "
+		;
+	
+	$arr_prepare =
+		array(
+			":limit_num"	=> $param_arr["limit_num"]
+			,":offset"		=> $param_arr["offset"]
+		);
 
-    $arr_prepare =
-        array(
-            ":limit_num"    => $param_arr["limit_num"]
-            ,":offset"      => $param_arr["offset"]
-        );
+	$conn = null;
+	try
+	{
+		db_conn( $conn );
+		$stmt = $conn->prepare( $sql );
+		$stmt->execute( $arr_prepare );
+		$result = $stmt->fetchAll();
+	}
+	catch( Exception $e )
+	{
+		return $e->getMessage();
+	}
+	finally
+	{
+		$conn = null;
+	}
 
-    $conn = null;
-    try
-    {
-        db_conn( $conn );
-        $stmt = $conn->prepare( $sql );
-        $stmt->execute( $arr_prepare );
-        $result = $stmt->fetchAll();
-    }
-    catch( Exception $e )
-    {
-        return $e->getMessage();
-    }
-    finally
-    {
-        $conn = null;
-    }
-
-    return $result;
-
+	return $result;
 }
 
+// ---------------------------------
+// 함수명	: select_board_info_cnt
+// 기능		: 게시판 정보 테이블 레코드 카운트 검색
+// 파라미터	: 없음
+// 리턴값	: Array		$result
+// ---------------------------------
 function select_board_info_cnt()
 {
-    $sql =
-    " SELECT "
-    ."      COUNT(*) cnt "
-    ." FROM "
-    ."      board_info "
-    ." WHERE "
-    ." board_del_flg = '0'"
-    ;
+	$sql =
+		" SELECT "
+		." 		COUNT(*) cnt "
+		." FROM "
+		." 		board_info "
+		." WHERE "
+		." 		board_del_flg = '0' "
+		;
+	$arr_prepare = array();
 
-    $arr_prepare =
-        array();
+	$conn = null;
+	try
+	{
+		db_conn( $conn );
+		$stmt = $conn->prepare( $sql );
+		$stmt->execute( $arr_prepare );
+		$result = $stmt->fetchAll();
+	}
+	catch( Exception $e )
+	{
+		return $e->getMessage();
+	}
+	finally
+	{
+		$conn = null;
+	}
 
-    $conn = null;
-    try
-    {
-        db_conn( $conn );
-        $stmt = $conn->prepare( $sql );
-        $stmt->execute( $arr_prepare );
-        $result = $stmt->fetchAll();
-    }
-    catch( Exception $e )
-    {
-        return $e->getMessage();
-    }
-    finally
-    {
-        $conn = null;
-    }
-
-    return $result;
+	return $result;
 }
 
+// ---------------------------------
+// 함수명	: select_board_info_no
+// 기능		: 게시판 특정 게시글 정보 검색
+// 파라미터	: Array		&$param_no
+// 리턴값	: Array		$result
+// ---------------------------------
+function select_board_info_no( &$param_no )
+{
+	$sql =
+		" SELECT "
+		."	board_no "
+		."	,board_title "
+		."	,board_contents "
+		." FROM "
+		."	board_info "
+		." WHERE "
+		."	board_no = :board_no "
+		;
 
-// TODO : test Start
-// $arr = 
-//     array(
-//         "limit_num" => 5
-//         ,"offset"    => 0
-//     );
-// $result = select_board_info_paging( $arr );
+	$arr_prepare =
+		array(
+			":board_no"	=> $param_no
+		);
 
-// print_r($result);
-// TODO : test End
+	$conn = null;
+	try
+	{
+		db_conn( $conn );
+		$stmt = $conn->prepare( $sql );
+		$stmt->execute( $arr_prepare );
+		$result = $stmt->fetchAll();
+	}
+	catch( Exception $e )
+	{
+		return $e->getMessage();
+	}
+	finally
+	{
+		$conn = null;
+	}
 
+	return $result[0]; // 조건을 PK로 걸어줘서, 리턴값이 1개만 있기 때문에 [0]을 적어줌.
+}
 
-?>
+// ---------------------------------
+// 함수명	: update_board_info_no
+// 기능		: 게시판 특정 게시글 정보 수정
+// 파라미터	: Array			&$param_arr
+// 리턴값	: INT/STRING	$result_cnt/ERRMSG
+// ---------------------------------
+function update_board_info_no( &$param_arr )
+{
+	$sql=
+	" UPDATE "
+	."	board_info "
+	." SET "
+	."	board_title = :board_title "
+	."	,board_contents = :board_contents"
+	." WHERE "
+	."	board_no = :board_no "
+	;
+	$arr_prepare =
+	array(
+		":board_title"		=> $param_arr["board_title"]
+		,":board_contents"	=> $param_arr["board_contents"]
+		,":board_no"		=> $param_arr["board_no"]
+	);
+	
+	$conn = null;
+	try
+	{
+		db_conn( $conn );
+		$conn->beginTransaction();
+		$stmt = $conn->prepare( $sql );
+		$stmt->execute( $arr_prepare );
+		$result_cnt = $stmt->rowCount();
+		$conn->commit();
+	}
+	catch( Exception $e )
+	{
+		$conn->rollback();
+		return $e->getMessage();
+	}
+	finally
+	{
+		$conn = null;
+	}
+
+	return $result_cnt;
+}
+
+// TODO : start
+// $i = 1;
+// print_r(select_board_info_no( $i ));
+// TODO : end
